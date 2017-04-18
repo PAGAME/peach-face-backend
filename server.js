192188b7c0a2ef2ae39a2b51a94362b8d5ed2115
byyMac <471073043@qq.com>
2017-04-18 12:55:04 +0800

    delete Cloudant

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
-var express = require("express");
+const express = require("express");
+const bodyParser = require('body-parser')
+
 var app = express();
-var cfenv = require("cfenv");
-var bodyParser = require('body-parser')
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
-
 // parse application/json
 app.use(bodyParser.json())
-
-var mydb;
 
 /* Endpoint to greet and add a new visitor to database.
 * Send a POST request to localhost:3000/api/visitors with body
@@ -19,18 +16,7 @@ var mydb;
 */
 app.post("/api/visitors", function (request, response) {
   var userName = request.body.name;
-  if(!mydb) {
-    console.log("No database.");
-    response.send("Hello " + userName + "!");
-    return;
-  }
-  // insert the username as a document
-  mydb.insert({ "name" : userName }, function(err, body, header) {
-    if (err) {
-      return console.log('[mydb.insert] ', err.message);
-    }
-    response.send("Hello " + userName + "! I added you to the database.");
-  });
+  response.send("Hello " + userName + "!");
 });
 
 /**
@@ -46,60 +32,14 @@ app.post("/api/visitors", function (request, response) {
  */
 app.get("/api/visitors", function (request, response) {
   var names = [];
-  if(!mydb) {
-    response.json(names);
-    return;
-  }
-
-  mydb.list({ include_docs: true }, function(err, body) {
-    if (!err) {
-      body.rows.forEach(function(row) {
-        if(row.doc.name)
-          names.push(row.doc.name);
-      });
-      response.json(names);
-    }
-  });
+  response.json(names);
 });
-
-
-// load local VCAP configuration  and service credentials
-var vcapLocal;
-try {
-  vcapLocal = require('./vcap-local.json');
-  console.log("Loaded local VCAP", vcapLocal);
-} catch (e) { }
-
-const appEnvOpts = vcapLocal ? { vcap: vcapLocal} : {}
-
-const appEnv = cfenv.getAppEnv(appEnvOpts);
-
-if (appEnv.services['cloudantNoSQLDB']) {
-  // Load the Cloudant library.
-  var Cloudant = require('cloudant');
-
-  // Initialize database with credentials
-  var cloudant = Cloudant(appEnv.services['cloudantNoSQLDB'][0].credentials);
-
-  //database name
-  var dbName = 'mydb';
-
-  // Create a new "mydb" database.
-  cloudant.db.create(dbName, function(err, data) {
-    if(!err) //err if database doesn't already exists
-      console.log("Created database: " + dbName);
-  });
-
-  // Specify the database we are going to use (mydb)...
-  mydb = cloudant.db.use(dbName);
-}
 
 //serve static file (index.html, images, css)
 app.use(express.static(__dirname + '/views'));
 
-
-
-var port = process.env.PORT || 3000
+// start app
+var port = process.env.PORT || 4000
 app.listen(port, function() {
     console.log("To view your app, open this link in your browser: http://localhost:" + port);
 });
